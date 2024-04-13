8de6905ce27802b21652c4fae262b58d94324525
Jacques Fourie <jacques@fourie.ca>
2024-04-13 15:53:30 -0600

    Refactor account

--- src/account.ts
+++ src/account.ts
@@ -1,6 +1,6 @@
-import { Document } from 'mongoose';
 import { keys } from 'ts-transformer-keys';
 import typia, { tags } from "typia";
+import { Document } from 'mongoose';
 
 export enum Roles {
     Unverified = 'unverified', // Unverified account created through registration form
@@ -8,13 +8,6 @@ export enum Roles {
     NonMember = 'nonmember', // Verified club member account
     Admin = 'admin' // Admin account
 }
-
-export type IAccountDocument = Omit<IAccount, 'id'> & {
-    hash: string;
-    sessionid: string;
-};
-
-export type IAccountForm = Omit<IAccount, 'id'>;
 
 export interface IAccount {
     id: string;
@@ -33,10 +26,15 @@ export interface ICredentials {
     password: string;
 }
 
+export type IAccountDocument = Omit<IAccount, 'id' | 'balance'> & Document & {
+    hash: string;
+    sessionid: string;
+};
+
+export type IAccountForm = Omit<IAccount, 'id' | 'balance'>;
+
 export const isIAccount = typia.createEquals<IAccount>();
-
 export const isIAccountForm = typia.createEquals<IAccountForm>();
-
 export const isICredentials = typia.createEquals<ICredentials>();
 
 export const keysIAccount = keys<IAccount>();
